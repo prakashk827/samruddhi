23b74c941fc2bb4b66e68f230ae4c4de70cab467
Prakash Koppal <prakashk827@gmail.com>
2021-08-07 13:18:18 +0530

    Added forget password contact page

---
+++ user/forgot-password.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!-- Font-icon css-->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Login </title>
+
+
+
+</head>
+<body>
+<section class="material-half-bg">
+    <div class="cover"></div>
+</section>
+<section class="lockscreen-content">
+    <div class="">
+       <h2 style="color:white">
+        <center>
+            Please contact to this number <br>
+            <a style="text-decoration:none;color:white;" href="tel:+91 8553621521">+91 8553621521</a>
+            to reset password. <br><br> <br><br> <br><br>
+        </center>
+       </h2>
+        <br><br>
+
+    </div>
+
+</section>
+<!-- Essential javascripts for application to work-->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+<!-- The javascript plugin to display page loading on top-->
+<script src="js/plugins/pace.min.js"></script>
+
+</body>
+</html>
