d716cb8cb6142834371e59a50081f2fc2326f798
Prakash Koppal <prakashk827@gmail.com>
2021-08-26 06:59:47 +0530

    Showing Lucky Number

--- user/show-all-purchased-coupons.php
+++ user/show-all-purchased-coupons.php
@@ -1,85 +1,82 @@
 <?php
-include_once ("../db/db.php");
+include_once("../db/db.php");
 session_start();
-if ($_SESSION["clientUId"] == '')
-{
-    header("Location:../index.php");
+if ($_SESSION["clientUId"] == '') {
+  header("Location:../index.php");
 }
 ?>
 
 <title>My Coupons</title>
-<?php include_once ("includes/head.php"); ?>
-
-  
-    <!-- Navbar starts-->
-    <?php include_once ("includes/navbar.php"); ?>
-    <!-- Navbar Ends-->
-    
-    
-    <!-- Sidebar menu starts-->
-    <?php include_once ("includes/sidebar.php"); ?>
-     <!-- Sidebar menu ends-->
+<?php include_once("includes/head.php"); ?>
 
 
-      <main class="app-content">
-      <div class="app-title">
-        <div>
-          <h1><i class="fa fa-tags"></i> Coupon Purchased History</h1>
-          <!-- <p>Showing all purchased coupons</p> -->
-        </div>
-        <ul class="app-breadcrumb breadcrumb side">
-          <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-          
-          <li class="breadcrumb-item active"><a href="dashboard.php">dashboard</a></li>
-        </ul>
-      </div>
+<!-- Navbar starts-->
+<?php include_once("includes/navbar.php"); ?>
+<!-- Navbar Ends-->
 
-       
-      <div class="row">
-      	<div class="col-md-2">
-      	<a href="show-all-coupons.php"><button class="btn btn-primary"><i class="fa fa-tag"></i> Buy Coupon</button></a><br><br>
-      	</div>
-      	
-      	<div class="col-md-2">
-      	<a href="my-coupons.php"><button class="btn btn-danger"><i class="fa fa-tags"></i> My Coupons</button></a><br><br>
-      	</div>	
-      	
-        <div class="col-md-12">
-          <div class="tile">
-            <div class="tile-body">
-              <div class="table-responsive">
-                <table class="table table-hover table-bordered" id="sampleTable">
-                  <thead>
-                    <tr>
-                      
-                      <th>Purchased Date</th>
-                      <th>Purchased Time </th>
-                      <th>Coupon Id</th>
-                      <th>Coupon Name</th>
-                      <th>Lucky Number</th>
-                      <th>Coupon Worth (Rs)</th>
-                      <th>Sale Back Amt. (Rs)</th>
-                      <th>Price (Rs)</th>
-                      <th>Qty</th>
-                      <th>Amount (Rs)</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-<?php
-$clientUId = $_SESSION['clientUId'];
-$query = "SELECT couponId,luckyNumber,couponWorth,salebackAmt,coupons_sold.time,boughtOn,couponName,couponPrice,paidAmt,boughtQty,paidAmt FROM coupons INNER JOIN coupons_sold ON coupons.id = couponId 
+
+<!-- Sidebar menu starts-->
+<?php include_once("includes/sidebar.php"); ?>
+<!-- Sidebar menu ends-->
+
+
+<main class="app-content">
+  <div class="app-title">
+    <div>
+      <h1><i class="fa fa-tags"></i> My Coupons</h1>
+      <!-- <p>Showing all purchased coupons</p> -->
+    </div>
+    <ul class="app-breadcrumb breadcrumb side">
+      <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
+
+      <li class="breadcrumb-item active"><a href="#">dashboard</a></li>
+    </ul>
+  </div>
+
+
+  <div class="row">
+    <div class="col-md-2">
+      <a href="show-all-coupons.php"><button class="btn btn-primary"><i class="fa fa-tag"></i> Buy Coupon</button></a><br><br>
+    </div>
+
+    <div class="col-md-2">
+      <a href="my-coupons.php"><button class="btn btn-danger"><i class="fa fa-tags"></i> My Coupons</button></a><br><br>
+    </div>
+
+    <div class="col-md-12">
+      <div class="tile">
+        <div class="tile-body">
+          <div class="table-responsive">
+            <table class="table table-hover table-bordered" id="sampleTable">
+              <thead>
+                <tr>
+
+                  <th>Purchased Date</th>
+                  <th>Purchased Time </th>
+                  <th>Coupon Id</th>
+                  <th>Coupon Name</th>
+                  <th>Lucky Number</th>
+                  <th>Coupon Worth (Rs)</th>
+                  <th>Sale Back Amt. (Rs)</th>
+                  <th>Price (Rs)</th>
+                  <th>Qty</th>
+                  <th>Amount (Rs)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php
+                $clientUId = $_SESSION['clientUId'];
+                $query = "SELECT couponId,luckyNumber,couponWorth,salebackAmt,coupons_sold.time,boughtOn,couponName,couponPrice,paidAmt,boughtQty,paidAmt FROM coupons INNER JOIN coupons_sold ON coupons.id = couponId 
 WHERE `clientUId`= '$clientUId' AND paymentStatus = 'complete' AND coupons_sold.status !='inactive'";
-$exe = mysqli_query($conn, $query);
-if (mysqli_num_rows($exe) > 0)
-{
-    while ($data = mysqli_fetch_assoc($exe))
-    {
-?>
-                 <tr>
-                     
+                $exe = mysqli_query($conn, $query);
+                if (mysqli_num_rows($exe) > 0) {
+                  while ($data = mysqli_fetch_assoc($exe)) {
+                ?>
+                    <tr>
+
                       <td><?php echo $data['boughtOn']; ?></td>
                       <td><?php echo $data['time']; ?></td>
-                       <td><?php echo $data['couponId']; ?></td>
+                      <td><?php echo $data['couponId']; ?></td>
                       <td class="payment"><?php echo $data['couponName']; ?></td>
                       <td class="payment"><?php echo $data['luckyNumber']; ?></td>
                       <td><?php echo $data['couponWorth']; ?></td>
@@ -87,96 +84,93 @@ if (mysqli_num_rows($exe) > 0)
                       <td><?php echo $data['couponPrice']; ?></td>
                       <td><?php echo $data['boughtQty']; ?></td>
                       <td><?php echo $data['paidAmt']; ?></td>
-                      
+
                     </tr>
-<?php
-    }
-?>
-        
-<?php
+                  <?php
+                  }
+                  ?>
 
-}
-else
-{
-    echo "No history found.";
-}
+                <?php
 
-?>
+                } else {
+                  echo "No history found.";
+                }
 
-                   
-                   
-                  </tbody>
-                </table>
-              </div>
-            </div>
+                ?>
+
+
+
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-    </main>
-    
-  
+    </div>
+  </div>
+</main>
 
 
-    <!-- Footer Starts-->
-     <?php include_once ("includes/footer.php"); ?>
-    <!-- Footer Ends-->
-    <!-- Data table plugin-->
-    <script type="text/javascript" src="js/plugins/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="js/plugins/dataTables.bootstrap.min.js"></script>
-    <script type="text/javascript">$('#sampleTable').DataTable();</script>
-    <script type="text/javascript" src="js/plugins/sweetalert.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://checkout.razorpay.com/v1/checkout.js"></script>
+
+
+<!-- Footer Starts-->
+<?php include_once("includes/footer.php"); ?>
+<!-- Footer Ends-->
+<!-- Data table plugin-->
+<script type="text/javascript" src="js/plugins/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="js/plugins/dataTables.bootstrap.min.js"></script>
+<script type="text/javascript">
+  $('#sampleTable').DataTable();
+</script>
+<script type="text/javascript" src="js/plugins/sweetalert.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://checkout.razorpay.com/v1/checkout.js"></script>
 
 <script>
+  $(document).ready(function() {
 
-$(document).ready(function(){
- 
-  $('.cancel').click(function(){
+    $('.cancel').click(function() {
       var id = $(this).attr("data-id");
-      
-        swal({
-          title: "Are you sure you want to delete this ? ",
-          text: "" ,
-          type: "warning",
-          showCancelButton: true,
-          confirmButtonText: "Yes",
-          cancelButtonText: "No",
-          closeOnConfirm: false,
-          closeOnCancel: false
-        }, function(isConfirm) {
-          if (isConfirm) {
 
-             $.post("delete/delete-payment-pending-coupon.php",
-          {
-            id:id
-          },
-          function(data)
-          {
-             swal("Deleted!", data , "success");
-               window.location.href="pending-payment.php";         
-          });
-             
-          } else {
-            swal("Cancelled", "", "error");
-          }
-        });
+      swal({
+        title: "Are you sure you want to delete this ? ",
+        text: "",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes",
+        cancelButtonText: "No",
+        closeOnConfirm: false,
+        closeOnCancel: false
+      }, function(isConfirm) {
+        if (isConfirm) {
+
+          $.post("delete/delete-payment-pending-coupon.php", {
+              id: id
+            },
+            function(data) {
+              swal("Deleted!", data, "success");
+              window.location.href = "pending-payment.php";
+            });
+
+        } else {
+          swal("Cancelled", "", "error");
+        }
       });
+    });
 
-//Payment
-  $('.payment').click(function(){
+    //Payment
+    $('.payment').click(function() {
 
-        var id = $(this).attr("data-id");
+      var id = $(this).attr("data-id");
 
-        window.location.href="cart-page.php?id="+id;
-        
-        
-});
+      window.location.href = "cart-page.php?id=" + id;
 
 
-});
+    });
 
+
+  });
 </script>
 
 </body>
+
 </html>
