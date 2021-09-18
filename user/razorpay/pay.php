4c863f2a1fcbffc36d5c76f1d2f2927e95b83b23
Prakash Koppal <prakashk827@gmail.com>
2021-09-18 17:12:42 +0530

    Added key and values in notes

--- user/razorpay/pay.php
+++ user/razorpay/pay.php
@@ -87,11 +87,22 @@ if (isset($_POST['couponId']) && isset($_POST['qty'])) {
     // We create an razorpay order using orders api
     // Docs: https://docs.razorpay.com/docs/orders
     //
+    
+
     $orderData = [
         'receipt' => $couponId.' | '.$clientUId,
         'amount' => $razorPayAmt * 100, // 2000 rupees in paise
         'currency' => 'INR',
-        'payment_capture' => 1 // auto capture
+        'payment_capture' => 1 ,// auto capture
+        'notes' => array(
+                    'couponId' => $couponId,
+                    'couponPrice'=>$couponPrice,
+                    'couponName' =>$couponName,
+                    'description' =>$description,
+                    'paiAmt'=>$paidAmt,
+                    'couponQty'=>$qty,
+                    'clientUId'=>$clientUId
+        )
     ];
     
     $razorpayOrder = $api->order->create($orderData);
